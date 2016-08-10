36e23be592dbc20d1f137772cb0e9df95359e543
Maxime Junger <maxime@junger.io>
2016-08-10 18:11:50 +0200

    Carthage

--- Example/MJSnackBar/ViewController.swift
+++ Example/MJSnackBar/ViewController.swift
@@ -10,65 +10,65 @@ import UIKit
 import MJSnackBar
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+	
 	var snackbar: MJSnackBar! = MJSnackBar()
-
+	
 	var dataArray = [
 		"Walking the dog",
 		"Take a shower",
 		"Clean house"
 	]
-
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
-
+		
 		self.edgesForExtendedLayout = .None
-
+		
 		var customSnackBar = Dictionary<String, Any>()
-//		customSnackBar["spaceOnSide"] = 5.0
-//		customSnackBar["spaceOnBottom"] = 5.0
-//		customSnackBar["snackViewHeight"] = 50.0
-//		customSnackBar["backgroundColor"] = 0x1D1D1D
-//		customSnackBar["backgroundAlpha"] = CGFloat(0.8)
-//		customSnackBar["appearanceDuration"] = 4.0
-//		customSnackBar["animationTime"] = 0.3
-//		customSnackBar["leftActionTextColor"] = 0xFFFFFF
-//		customSnackBar["actionButtonText"] = "Undo"
+		//		customSnackBar["spaceOnSide"] = 5.0
+		//		customSnackBar["spaceOnBottom"] = 5.0
+		//		customSnackBar["snackViewHeight"] = 50.0
+		//		customSnackBar["backgroundColor"] = 0x1D1D1D
+		//		customSnackBar["backgroundAlpha"] = CGFloat(0.8)
+		//		customSnackBar["appearanceDuration"] = 4.0
+		//		customSnackBar["animationTime"] = 0.3
+		//		customSnackBar["leftActionTextColor"] = 0xFFFFFF
+		//		customSnackBar["actionButtonText"] = "Undo"
 		customSnackBar["actionButtonTextColorNormal"] = 0x00FF00
-//		customSnackBar["actionButtonTextColorSelected"] = 0xFF0000
-
+		//		customSnackBar["actionButtonTextColorSelected"] = 0xFF0000
+		
 		// snackbar = MJSnackBar(custom: customSnackBar)
 		
 		snackbar = MJSnackBar(type: MJSnackBar.SnackType.ANDROID)
 		snackbar.addCustomStyle(customSnackBar)
 	}
-
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
+	
 	func numberOfSectionsInTableView(tableView: UITableView) -> Int {
 		return 1
 	}
-
+	
 	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return dataArray.count
 	}
-
+	
 	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 		let cell = UITableViewCell()
-
+		
 		cell.textLabel?.text = dataArray[indexPath.row]
-
+		
 		return cell
 	}
-
+	
 	func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
 		return true
 	}
-
+	
 	func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
 		if editingStyle == UITableViewCellEditingStyle.Delete {
 			let savedString = dataArray[indexPath.row]
@@ -90,7 +90,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 			})
 		}
 	}
-
+	
 	func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
 		tableView.deselectRowAtIndexPath(indexPath, animated: true)
 	}
---
+++ MJSnackBar/MJSnackBar/MJSnackBar.h
@@ -0,0 +1,19 @@
+//
+//  MJSnackBar.h
+//  MJSnackBar
+//
+//  Created by Maxime Junger on 09/08/16.
+//  Copyright © 2016 Shakarang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MJSnackBar.
+FOUNDATION_EXPORT double MJSnackBarVersionNumber;
+
+//! Project version string for MJSnackBar.
+FOUNDATION_EXPORT const unsigned char MJSnackBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MJSnackBar/PublicHeader.h>
+
+
